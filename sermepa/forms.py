12fa4560dbbc55581af4e3eec68c89680655f144
Biel <bielalgaida@gmail.com>
2015-11-12 10:41:32 +0100

    firma sha256

--- sermepa/forms.py
+++ sermepa/forms.py
@@ -1,62 +1,46 @@
+# -*- encoding: utf-8 -*-
 from __future__ import unicode_literals
+import json
+
 from django import forms
 from django.conf import settings
 from django.utils.safestring import mark_safe
 
-import hashlib
-
 from .models import SermepaResponse
+from .mixins import SermepaMixin
 
-class SermepaPaymentForm(forms.Form):
 
-    Ds_Merchant_Currency = forms.IntegerField(widget=forms.HiddenInput())
-    Ds_Merchant_Titular = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_MerchantName = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_ProductDescription = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_MerchantData = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_MerchantURL = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_TransactionType = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_Amount = forms.IntegerField(widget=forms.HiddenInput())
-    Ds_Merchant_MerchantSignature = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_Terminal = forms.IntegerField(widget=forms.HiddenInput())
-    Ds_Merchant_MerchantCode = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_AuthorisationCode = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_Order = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_UrlOK = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_UrlKO = forms.CharField(widget=forms.HiddenInput())
-    Ds_Merchant_Identifier = forms.CharField(widget=forms.HiddenInput(), required=False)
-    Ds_Merchant_Group = forms.CharField(widget=forms.HiddenInput(), required=False)
-    Ds_Merchant_DirectPayment = forms.CharField(widget=forms.HiddenInput(), required=False)
+class SermepaPaymentForm(SermepaMixin, forms.Form):
+    Ds_SignatureVersion = forms.IntegerField(widget=forms.HiddenInput())
+    Ds_MerchantParameters = forms.IntegerField(widget=forms.HiddenInput())
+    Ds_Signature = forms.IntegerField(widget=forms.HiddenInput())
 
     def __init__(self, *args, **kwargs):
+        merchant_parameters = kwargs.pop('merchant_parameters', None)
         super(SermepaPaymentForm, self).__init__(*args, **kwargs)
+        if merchant_parameters:
+            json_data = json.dumps(merchant_parameters)
+            order = merchant_parameters['DS_MERCHANT_ORDER']
+            b64_params = self.encode_base64(json_data)
+            signature = self.get_firma_peticion(order, b64_params,
+                                                settings.SERMEPA_SECRET_KEY)
 
-        SECRET_KEY = settings.SERMEPA_SECRET_KEY
-
-        key = '%s%s%s%s%s%s%s%s%s%s' % (self.initial['Ds_Merchant_Amount'],
-                                  self.initial['Ds_Merchant_Order'],
-                                  self.initial['Ds_Merchant_MerchantCode'],
-                                  self.initial['Ds_Merchant_Currency'],
-                                  self.initial['Ds_Merchant_TransactionType'],
-                                  self.initial['Ds_Merchant_MerchantURL'],
-                                  self.initial.get('Ds_Merchant_Identifier') or '',
-                                  self.initial.get('Ds_Merchant_Group') or '',
-                                  self.initial.get('Ds_Merchant_DirectPayment') or '',
-                                  SECRET_KEY,)
-        sha1 = hashlib.sha1(key.encode('utf-8'))
-        self.initial['Ds_Merchant_MerchantSignature'] = sha1.hexdigest().upper()
+            self.initial['Ds_SignatureVersion'] = settings.SERMEPA_SIGNATURE_VERSION
+            self.initial['Ds_MerchantParameters'] = b64_params
+            self.initial['Ds_Signature'] = signature
 
     def render(self):
         return mark_safe(u"""<form id="tpv_form" action="%s" method="post">
             %s
-            <input type="image" src="%s" border="0" name="submit" alt="Comprar ahora" />
-        </form>""" % (settings.SERMEPA_URL_PRO, self.as_p(), settings.SERMEPA_BUTTON_IMG))
+            <input type="submit" name="submit" alt="Comprar ahora" value="Comprar ahora"/>
+        </form>""" % (settings.SERMEPA_URL_PRO, self.as_p()))
 
     def sandbox(self):
         return mark_safe(u"""<form id="tpv_form" action="%s" method="post">
             %s
-            <input type="image" src="%s" border="0" name="submit" alt="Comprar ahora" />
-        </form>""" % (settings.SERMEPA_URL_TEST, self.as_p(), settings.SERMEPA_BUTTON_IMG))
+            <input type="submit" name="submit" alt="Comprar ahora" value="Comprar ahora"/>
+        </form>""" % (settings.SERMEPA_URL_TEST, self.as_p()))
+
 
 class SermepaResponseForm(forms.ModelForm):
     Ds_Date = forms.DateField(required=False, input_formats=('%d/%m/%Y',))
@@ -65,4 +49,3 @@ class SermepaResponseForm(forms.ModelForm):
     class Meta:
         model = SermepaResponse
         exclude = ()
-
